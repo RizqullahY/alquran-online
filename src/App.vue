cdb895a5331feec104759feab567a0dc48031b80
Rizqullah Yusuf <rizqullahyusufrafly@gmail.com>
2025-02-28 17:20:19 +0700

    LAS

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,9 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    <div class="">
-      <nav>
+    <div class="w-full bg-sky-400 p-5 flex justify-between">
+      <h1 class="text-2xl font-mono font-bold italic">RMD</h1>
+      <nav class="w-40 flex justify-around">
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
